ebddd3921b946923d7deb81566b62255efec22c2
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2022-06-26 15:34:48 -0300

    Atualiza projeto de classificação de tomates com documentação
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>

--- chapter10-neural_network_fundamentals/keras_animals.py
+++ chapter10-neural_network_fundamentals/keras_animals.py
@@ -1,25 +1,22 @@
 # USAGE
 # python keras_animals.py --output output/keras_animals.png
-from sys import displayhook
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.preprocessing import normalize
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.datasets import cifar10
-from pyimagesearch.preprocessing import SimplePreprocessor
-from pyimagesearch.datasets import SimpleDatasetLoader
-from imutils import paths
 import argparse
-import numpy as np
-import math
+from sys import displayhook
+
 import matplotlib.pyplot as plt
-from IPython.display import clear_output
-from matplotlib.pyplot import figure
+import numpy as np
+from imutils import paths
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import (LabelBinarizer, LabelEncoder,
+                                   StandardScaler, normalize)
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import SGD
+
+from pyimagesearch.datasets import SimpleDatasetLoader
+from pyimagesearch.preprocessing import SimplePreprocessor
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -27,17 +24,30 @@ ap.add_argument("-o", "--output", required=True,
 	help="path to the output loss/accuracy plot")
 args = vars(ap.parse_args())
 
-imagePaths = list(paths.list_images('..\\datasets\\animals'))
-#imagePaths = list(paths.list_images('..\\datasets\\TOMATES_LADOS_CORES'))
+imagePaths = list(paths.list_images('./datasets/tomates/TOMATES_LADOS_CORES'))
+''' O caminho das imagens a serem usadas para treinar a rede neural '''
 
 # initialize the image preprocessor, load the dataset from disk,
 # and reshape the data matrix
-sp = SimplePreprocessor(32, 32)
+width = 8
+''' a largura para converter a imagem '''
+
+height = 8
+''' a altura para converter a imagem '''
+
+sp = SimplePreprocessor(width, height)
 sdl = SimpleDatasetLoader(preprocessors=[sp])
 (data, labels) = sdl.load(imagePaths, verbose=500)
 print('Shape {}'.format(data.shape))
 print('Shape[0] {}'.format(data.shape[0]))
-data = data.reshape((data.shape[0], 3072))
+
+area = width * height
+''' A área total da imagem '''
+
+size = 3 * area
+''' O tamanho total da entrada (tamanho da imagem) * 3(os três canais de cores do RGB) '''
+
+data = data.reshape((data.shape[0], size))
 
 print(data[0])
 
@@ -51,7 +61,9 @@ normalizar = True
 if (normalizar):
     # Aqui os valores dos canais de cores ficam entre zero e um
     #dataNormal = normalize(data)
-    dataNormal = data.astype("float") / 255.0
+    rgb_size = 255.0
+    ''' O tamanho total da faixa de cores suportadda pelo RGB '''
+    dataNormal = data.astype("float") / rgb_size
     print(dataNormal[0])
     # partition the data into training and testing splits using 75% of
     # the data for training and the remaining 25% for testing
@@ -77,17 +89,23 @@ else:
     
 # define the 3072-1024-512-3 architecture using Keras
 model = Sequential()
-model.add(Dense(1024, input_shape=(3072,), activation="relu"))
-model.add(Dense(512, activation="relu"))
-model.add(Dense(3, activation="softmax"))
+model.add(Dense(area , input_shape=(size,), activation="relu"))
+
+model.add(Dense(area/2, activation="relu"))
+
+output_size = 3
+''' A quantidade de classes para saída, a função de ativação deverá ser o softmax para conseguir definir qual será a classe de saída. '''
+
+model.add(Dense(output_size, activation="softmax"))
 
 # train the model using SGD
 print("[INFO] training network...")
 sgd = SGD(0.01)
 model.compile(loss="categorical_crossentropy", optimizer=sgd,
 	metrics=["accuracy"])
+epoch_size = 100
 H = model.fit(trainX, trainY, validation_data=(testX, testY),
-	epochs=100, batch_size=32)
+	epochs=epoch_size, batch_size=32)
 
 # evaluate the network
 print("[INFO] evaluating network...")
@@ -98,10 +116,10 @@ print(classification_report(testY.argmax(axis=1),
 # plot the training loss and accuracy
 plt.style.use("ggplot")
 plt.figure()
-plt.plot(np.arange(0, 100), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, 100), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, 100), H.history["accuracy"], label="train_acc")
-plt.plot(np.arange(0, 100), H.history["val_accuracy"], label="val_acc")
+plt.plot(np.arange(0, epoch_size), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, epoch_size), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, epoch_size), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, epoch_size), H.history["val_accuracy"], label="val_acc")
 plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
--- chapter10-neural_network_fundamentals/pyimagesearch/preprocessing/simplepreprocessor.py
+++ chapter10-neural_network_fundamentals/pyimagesearch/preprocessing/simplepreprocessor.py
@@ -1,4 +1,5 @@
 # import the necessary packages
+import cv2
 class SimplePreprocessor:
 	def __init__(self, width, height, inter=cv2.INTER_AREA):
 		# store the target image width, height, and interpolation
